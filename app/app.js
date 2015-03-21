324db31412b464930a9ce1bc10602c2b4d781b22
msalas74 <mat.salas@comcast.net>
2015-03-20 21:12:45 -0700

    route and portal page

---
+++ app/app.js
@@ -0,0 +1,57 @@
+var app = angular.module("myApp", ['ngRoute', 'ngAnimate']);
+
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: './login.html',
+		controller: 'homeCtrl'
+	})
+	.when('/voters', {
+		templateUrl: './home.html',
+		controller: 'homeCtrl'
+	})
+	.when('/politicianbox', {
+		templateUrl: './home.html',
+		controller: 'homeCtrl'
+	})
+	.when('/interest-group', {
+		templateUrl: './home.html',
+		controller: 'homeCtrl'
+	})
+	.when('/press', {
+		templateUrl: './home.html',
+		controller: 'homeCtrl'
+	})
+	.when ('/error', {
+			template : '<p class="errorMsg">OOPS! - Seems like this page doesn\'t exist!</p>'
+	})
+	.otherwise ({
+			redirectTo : '/error'
+	});
+
+}]);
+
+app.run(function($rootScope, $location, $timeout) {
+	$rootScope.$on('$routeChangeError', function() {
+		$location.path('/error');
+	});
+	$rootScope.$on('$routeChangeStart', function() {
+		$rootScope.isLoading = true;
+	});
+	$rootScope.$on('$routeChangeSuccess', function() {
+		$timeout(function() {
+			$rootScope.isLoading = false;
+		}, 1000);
+	});
+});
+
+app.controller ('homeCtrl', function() {
+
+});
+
+
+
+
+
+
+
+
